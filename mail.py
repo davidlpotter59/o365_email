8fce89f906dae66404318bf1d9f51cd59f89313b
David Potter <davep@davidlpotter.com>
2019-11-17 17:01:16 -0500

    added attachment logic

--- mail.py
+++ mail.py
@@ -1,21 +1,47 @@
 import smtplib, os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.utils import formatdate
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email.mime.text import MIMEText
+from email import encoders
 
 sendingUser='dpotter@scips.com'
 sendingPassword='Kathar!na1959'
 toMail='linux@davidlpotter.com'
+cc='hdroadglide@gmail.com'  # not used yet - not sure it ever has to be used
 subject="Python Test Email"
+
+file = 'Setting up git.docx'
+fileDescription= file.split('.')[0]
+
+msg = MIMEMultipart()
+msg['From'] = sendingUser
+msg['To'] = toMail
+msg['Cc'] = cc
+msg['Date'] = formatdate(localtime = True)
+msg['Subject'] = 'Sending emails from Python'
+
 
 def send_email():
     try:    
         # Create message container - the correct MIME type is multipart/alternative.
-        msg = MIMEMultipart('alternative')
+        # msg = MIMEMultipart('alternative')
         
-        msg['From'] = sendingUser
-        msg['To'] = toMail        
-        msg['Subject'] = subject
+        # msg['From'] = sendingUser
+        # msg['To'] = toMail        
+        # msg['Subject'] = subject
 
+        fp = open(file, 'rb')
+        part = MIMEBase('application','vnd.ms-excel')
+        part.set_payload(fp.read())
+        fp.close()
+
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment', filename=fileDescription)
+        msg.attach(part)
         # see the code below to use template as body
         body_text = "Python says Hi this is body text of email"
         body_html = "<p><h1>Python says Hi this is body text of email</h1></p>"
@@ -28,20 +54,23 @@ def send_email():
         # Attach parts into message container.
         # According to RFC 2046, the last part of a multipart message, in this case
         # the HTML message, is best and preferred.
+        # msg.attach(part)
         msg.attach(part1)
         msg.attach(part2)
         
         # Send the message via local SMTP server.
         
+        
         mail = smtplib.SMTP("smtp.outlook.office365.com", 587, timeout=20)
-
-        # if tls = True                
+        
+        # if tls = True Microsoft does not support TLS this way - so leave it commented                 
         mail.starttls()        
 
         recepient = [toMail]
                     
         mail.login(sendingUser, sendingPassword)        
-        mail.sendmail(sendingUser, recepient, msg.as_string())        
+        mail.sendmail(sendingUser, recepient, msg.as_string())  
+        
         mail.quit()
         
     except Exception as e:
---
+++ test1.py
@@ -0,0 +1,4 @@
+file = 'Setting up git.docx'
+fileDescription= file.split('.')[0]
+
+print(fileDescription)
